d8f37abb245eb904bfad2f58d0a25dfeb0b3b00c
Simon Ngugi <snmwanjiru@gmail.com>
2023-07-03 04:05:15 -0700

    testing mike test

---
+++ jetpack/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Wazee walipitia happa");
+Console.WriteLine("Hii kiti iko wapi");
+int serdo=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(serdo);
