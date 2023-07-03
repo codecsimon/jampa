7455182197cd1ed46a57bcd1e9a13c5cd1a175d9
Simon Ngugi <snmwanjiru@gmail.com>
2023-07-03 03:43:19 -0700

    hee hee kali sana

---
+++ call.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class call
+{
+public int amount{get;set;}
+public string? productname{get;set;}
+
+public void getbal(int amt,string prod)
+{
+amount=amt;
+productname=prod;
+
+
+}
+
+}
+
